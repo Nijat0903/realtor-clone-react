e7b5d357cf2832fc235798a24522d7326061d78b
Nijat0903 <2979313825@qq.com>
2023-10-12 10:05:58 +0800

    create pages and routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Offers from './pages/Offers';
+import ForgotPassword from './pages/ForgotPassword';
+import Profile from './pages/Profile';
 
 function App() {
   return(
-    <div>
-      app
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={ <Home/> }/>
+          <Route path='/sign-in' element={ <SignIn/> }/>
+          <Route path='/sign-up' element={ <SignUp/> }/>
+          <Route path='/profiles' element={ <Profile/> }/>
+          <Route path='/offers' element={ <Offers/> }/>
+          <Route path='/forgot-password' element={ <ForgotPassword/> }/>
+        </Routes>
+      </Router>
+    </>
   )
 }
 
